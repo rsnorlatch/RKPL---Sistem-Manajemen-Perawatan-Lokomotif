c70e7ee76755bc56a1c32ac8377135868af2f2ba
rsnorlatch <muhfarisnew@gmail.com>
2025-10-29 12:46:54 +0700

    added factory function to create usecase with mocked dependencies

--- application/locomotive_calling_usecase.php
+++ application/locomotive_calling_usecase.php
@@ -1,8 +1,25 @@
 <?php
+include "../persistent/inmemory_locomotive_call_queue.php";
+include "../persistent/inmemory_locomotive_repository.php";
+
 class LocomotiveCallingUsecase
 {
 	public readonly ILocomotiveRepository $locomotive;
 	public readonly ILocomotiveCallQueue $call_queue;
+
+	private function __construct(ILocomotiveRepository $locomotive, ILocomotiveCallQueue $call_queue)
+	{
+		$this->locomotive = $locomotive;
+		$this->call_queue = $call_queue;
+	}
+
+	public static function create_mocked(array $mock_db, array $mock_queue)
+	{
+		return new LocomotiveCallingUsecase(
+			new InMemoryLocomotiveRepository($mock_db),
+			new InMemoryLocomotiveCallQueue($mock_queue)
+		);
+	}
 
 	public function call_locomotive(string $locomotive_id)
 	{
