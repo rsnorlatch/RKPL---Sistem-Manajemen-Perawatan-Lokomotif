fc8cc828d9f8f595c964c1be563275d908259d9e
rsnorlatch <muhfarisnew@gmail.com>
2026-02-28 20:30:03 +0700

    before marge

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gdm/lms',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd42289861c4926d1996708879f27cd4f55565392',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gdm/lms' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd42289861c4926d1996708879f27cd4f55565392',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
