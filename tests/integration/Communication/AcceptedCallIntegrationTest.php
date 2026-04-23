43620c29ba8950b50202e46933d358037aade7ea
Titaniums0079 <thompsondeanicsix@gmail.com>
2026-04-23 09:27:03 +0700

    update AcceptedCallIntegrationTest

--- tests/integration/Communication/AcceptedCallIntegrationTest.php
+++ tests/integration/Communication/AcceptedCallIntegrationTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use lms\feature\communication\persistence\MySqlAcceptedCallRepository;
+use lms\feature\communication\persistence\MySqlCallRepository;
+use lms\feature\signup\persistence\MySqlDriverRepository;
 use PHPUnit\Framework\TestCase;
 
 final class AcceptedCallIntegrationTest extends TestCase
@@ -11,20 +13,80 @@ final class AcceptedCallIntegrationTest extends TestCase
     {
         parent::setUp();
         $this->db = new \mysqli("localhost", "root", "", "lms_test");
+        $this->db->begin_transaction();
     }
 
 
-    public function testCreatedAcceptedCall_ShouldBeRetrievedableFromStorage()
+    public function testCreatedAcceptedCall_ShouldBeRetrieveable()
     {
+        $calls = new MySqlCallRepository($this->db);
+        $drivers = new MySqlDriverRepository($this->db);
         $accepted_calls = new MySqlAcceptedCallRepository($this->db);
+
+        $driver_id = $drivers->count() + 1;
+        $call_id = $calls->count() + 1;
+        $accepted_calls_id = $accepted_calls->count() + 1;
+
+        $drivers->insert($driver_id, "username", "email", "password");
+        $calls->insert($call_id, $driver_id, new DateTime());
+        $accepted_calls->insert($accepted_calls_id, $call_id);
+
+
+        $retrieved = $calls->get($call_id);
+
+
+        $this->assertNotNull($retrieved);
     }
 
+    public function testCreatedAcceptedCall_ShouldBeUpdateable()
+    {
+        $calls = new MySqlCallRepository($this->db);
+        $drivers = new MySqlDriverRepository($this->db);
+        $accepted_calls = new MySqlAcceptedCallRepository($this->db);
+
+        $driver_id = $drivers->count() + 1;
+        $call_id = $calls->count() + 1;
+        $accepted_calls_id = $accepted_calls->count() + 1;
+
+        $drivers->insert($driver_id, "username", "email", "password");
+        $drivers->insert($driver_id + 1, "username", "email", "password");
+        $calls->insert($call_id, $driver_id, new DateTime());
+        $calls->insert($call_id + 1, $driver_id + 1, new DateTime());
+        $accepted_calls->insert($accepted_calls_id, $call_id);
+
+
+        $accepted_calls->update($accepted_calls_id, $call_id + 1);
+
+
+        $this->assertEquals($call_id + 1, $accepted_calls->get($accepted_calls_id)->call_id);
+    }
+
+    public function testCreatedAcceptedCall_ShouldBeDeletable()
+    {
+        $calls = new MySqlCallRepository($this->db);
+        $drivers = new MySqlDriverRepository($this->db);
+        $accepted_calls = new MySqlAcceptedCallRepository($this->db);
+
+        $driver_id = $drivers->count() + 1;
+        $call_id = $calls->count() + 1;
+        $accepted_calls_id = $accepted_calls->count() + 1;
+
+        $drivers->insert($driver_id, "username", "email", "password");
+        $calls->insert($call_id, $driver_id, new DateTime());
+        $accepted_calls->insert($accepted_calls_id, $call_id);
+
+
+        $accepted_calls->delete($accepted_calls_id);
+
+
+        $this->assertEquals(null, $accepted_calls->get($accepted_calls_id));
+    }
 
     public function tearDown(): void
     {
         parent::tearDown();
 
-        $stmt = $this->db->prepare("DELETE FROM AcceptedCall");
-        $stmt->execute();
+        $this->db->rollback();
+        $this->db->close();
     }
 }
